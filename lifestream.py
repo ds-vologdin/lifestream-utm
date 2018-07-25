545be56a4e9335dadcb53ea49bb83c4ba386a25d
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-25 08:41:59 +0300

    результаты запросов в nametuple

--- lifestream.py
+++ lifestream.py
@@ -1,18 +1,20 @@
 import psycopg2
+from collections import namedtuple
 
 from private_settings import DATEBASE
 
 
 def get_status_tv_users_utm():
-    print(DATEBASE)
+
     con = psycopg2.connect(
         'host={host} port={port} dbname={db} user={user} password={passwd}'.format(
             **DATEBASE
         )
     )
     cur = con.cursor()
+    # группа с id 5003 - абоненты с услугой ТВ
     sql = '''SELECT t5.login, t5.full_name, t1.balance, t2.block_type,
-    t2.start_date, t2.expire_date, t2.is_deleted, t4.group_id
+    t2.start_date, t2.expire_date, t2.is_deleted
     FROM accounts t1
     LEFT OUTER JOIN blocks_info t2 ON (t1.id = t2.account_id AND
     t2.start_date = (SELECT MAX(start_date)
@@ -24,16 +26,23 @@ def get_status_tv_users_utm():
     '''
     cur.execute(sql)
     users_status = cur.fetchall()
+    UserStatistic = namedtuple(
+        'UserStatistic',
+        ['login', 'full_name', 'balance', 'block_type', 'start_date',
+         'expire_date', 'is_deleted'],
+        verbose=True)
+    users_status = list(map(UserStatistic._make, users_status))
     return users_status
 
 
 def get_status_tv_users_lifestream(): ...
 
 
-def find_change_status_to_lifestream(): ...
+def find_change_status_to_lifestream(utm_status_users, lifestream_status_user):
+    ...
 
 
-def apply_change_status_lifestream(): ...
+def apply_change_status_lifestream(status_change): ...
 
 
 def main():
