4f97c2b35aa00c125c55ce9e5e3cc5b9ce5eb6de
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-10-09 16:17:53 +0300

    _check_relations_utm_lifestream() is a pure function

--- lifestream-utm/relation_utm_lifestream.py
+++ lifestream-utm/relation_utm_lifestream.py
@@ -27,31 +27,41 @@ def find_user_in_lifestream(utm_user, lifestream_status_users):
             return lifestream_user
 
 
-def check_relations_utm_lifestream(utm_status_users, lifestream_status_users):
+def _check_relations_utm_lifestream(utm_status_users, lifestream_status_users):
+    no_relations_in_utm = []
     for user_lifestream in lifestream_status_users:
         found_users_utm = find_users_in_utm(user_lifestream, utm_status_users)
         if not len(found_users_utm) != 1:
             continue
+        no_relations_in_utm.append((user_lifestream, found_users_utm))
 
+    no_relations_in_lifestream = []
+    for user_utm in utm_status_users:
+        if not find_user_in_lifestream(user_utm, lifestream_status_users):
+            no_relations_in_lifestream.append(user_utm)
+    return no_relations_in_utm, no_relations_in_lifestream
+
+
+def check_relations_utm_lifestream(utm_status_users, lifestream_status_users):
+    no_relations_in_utm, no_relations_in_lifestream = _check_relations_utm_lifestream(
+        utm_status_users, lifestream_status_users)
+    for user_lifestream, users_utm in no_relations_in_utm:
         user_info_str = 'id: {} ; username: {} ({})'.format(
             user_lifestream['id'],
             user_lifestream['username'],
             user_lifestream['info']['fio'],
         )
-        if found_users_utm:
-            user_info_str += ' -> {}'.format(
-                ', '.join(map(str, found_users_utm))
-            )
+        if users_utm:
+            user_info_str += ' -> {}'.format(', '.join(map(str, users_utm)))
         else:
             user_info_str += ' -> не найдено'
         logger.warning(user_info_str)
 
-    for user_utm in utm_status_users:
-        if not find_user_in_lifestream(user_utm, lifestream_status_users):
-            logger.warning(
-                'id: {} ; utm full name: {} ({}) -> не найден в lifestream'.format(
-                    user_utm.lifestream_id, user_utm.full_name, user_utm.login
-                ))
+    for user_utm in no_relations_in_lifestream:
+        logger.warning(
+            'id: %s ; utm full name: %s (%s) -> не найден в lifestream',
+            user_utm.lifestream_id, user_utm.full_name, user_utm.login
+        )
 
 
 def get_users_utm_with_id_lifestream(utm_status_users):
