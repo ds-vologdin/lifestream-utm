f850dff9d0e0699bf315426d7746f08e5b3c8fa3
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-10-09 13:51:34 +0300

    test for get_lifestream_user

--- lifestream-utm/test_relation_utm_lifestream.py
+++ lifestream-utm/test_relation_utm_lifestream.py
@@ -53,3 +53,58 @@ def test_get_users_utm_with_id_lifestream():
     assert len(user_with_id_lifestream) == 1
     assert user_with_id_lifestream[0].login == '11278'
     assert user_with_id_lifestream[0].lifestream_id == '59e76927eea9ae0ce20c222e'
+
+
+@pytest.fixture(
+    params=[
+        ('567bc88ce2a6fe09ce000111', True),
+        ('567bc88ce2a6fe09ce000110', False),
+    ],
+    ids=[
+        'find id in lifestream user',
+        'not find id in lifestream user',
+    ]
+)
+def id_lifestream_params(request):
+    return request.param
+
+
+@pytest.fixture
+def lifestream_status_users():
+    return [
+        {
+            'updated': '2018-10-09 08:01:07.197000+03:00',
+            'id': '567bc88ce2a6fe09ce000111', 'city': '',
+            'email': 'anton@mail.ru', 'username': 'test1',
+            'info': {
+                'fio': 'Учетка для теста', 'activation_date': '24.12.15', 'period': '',
+                'address': 'г.Киров'
+            },
+            'subscriptions': [{'id': '109'}], 'is_blocked': False,
+            'created': '2015-12-24 13:27:24.189000+03:00'
+        },
+        {
+            'updated': '2018-10-09 08:01:07.197000+03:00',
+            'id': '567bc88ce2a6fe09ce000112', 'city': '',
+            'email': 'anton@mail.ru', 'username': 'test2',
+            'info': {
+                'fio': 'Учетка для теста', 'activation_date': '24.12.15', 'period': '',
+                'address': 'г.Киров'
+            },
+            'subscriptions': [{'id': '109'}], 'is_blocked': False,
+            'created': '2015-12-24 13:27:24.189000+03:00'
+        },
+    ]
+
+
+def test_get_lifestream_user(id_lifestream_params, lifestream_status_users):
+    id_lifestream, result = id_lifestream_params
+    user = relation_utm_lifestream.get_lifestream_user(
+        id_lifestream, lifestream_status_users)
+    assert bool(user) == result
+
+
+def test_get_lifestream_user_correct(lifestream_status_users):
+    user = relation_utm_lifestream.get_lifestream_user(
+        '567bc88ce2a6fe09ce000112', lifestream_status_users)
+    assert user['username'] == 'test2'
