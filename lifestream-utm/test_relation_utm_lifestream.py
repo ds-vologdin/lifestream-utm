3713975615475a6a3dbe343a548c392f3b6099f9
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-10-09 15:35:54 +0300

    test for find_change_status_to_lifestream()

--- lifestream-utm/test_relation_utm_lifestream.py
+++ lifestream-utm/test_relation_utm_lifestream.py
@@ -7,10 +7,10 @@ from utm_requests import UserStatus
 
 @pytest.fixture(
     params=[
-        ((datetime.datetime.now().timestamp()-600, 0), True),
-        ((datetime.datetime.now().timestamp()-600, 1), True),
-        ((datetime.datetime.now().timestamp()+600, 0), False),
-        ((datetime.datetime.now().timestamp()+600, 1), True),
+        ((datetime.datetime.now().timestamp()-600, False), True),
+        ((datetime.datetime.now().timestamp()-600, True), True),
+        ((datetime.datetime.now().timestamp()+600, False), False),
+        ((datetime.datetime.now().timestamp()+600, True), True),
     ],
     ids=[
         'last_block_expire_date < now and not deleted',
@@ -21,6 +21,36 @@ from utm_requests import UserStatus
 )
 def active_user_params(request):
     return request.param
+
+
+@pytest.fixture
+def utm_users():
+    return [
+        UserStatus(
+            login='11278', full_name='User1', balance=184.62, block_type=1,
+            last_block_start_date=1538342370, last_block_expire_date=datetime.datetime.now().timestamp()+600,
+            last_block_is_deleted=False, user_id=1146, lifestream_id='567bc88ce2a6fe09ce000111',
+            tarifs_id=[1311, 1517]
+        ),
+        UserStatus(
+            login='11272', full_name='User2', balance=184.62, block_type=1,
+            last_block_start_date=1538342370, last_block_expire_date=datetime.datetime.now().timestamp(),
+            last_block_is_deleted=True, user_id=1146, lifestream_id=None,
+            tarifs_id=[1311, 1517]
+        ),
+        UserStatus(
+            login='11233', full_name='User3', balance=184.62, block_type=1,
+            last_block_start_date=1538342370, last_block_expire_date=datetime.datetime.now().timestamp(),
+            last_block_is_deleted=True, user_id=1146, lifestream_id='567bc88ce2a6fe09ce000112',
+            tarifs_id=[1311, 1517, 1716]
+        ),
+        UserStatus(
+            login='12433', full_name='User4', balance=184.62, block_type=1,
+            last_block_start_date=1538342370, last_block_expire_date=datetime.datetime.now().timestamp(),
+            last_block_is_deleted=True, user_id=1146, lifestream_id='567bc88ce2a6fe09ce000113',
+            tarifs_id=[1311, 1517, 1716]
+        ),
+    ]
 
 
 def test_is_active_utm_user(active_user_params):
@@ -34,25 +64,11 @@ def test_is_active_utm_user(active_user_params):
     assert relation_utm_lifestream.is_active_utm_user(user) is result
 
 
-def test_get_users_utm_with_id_lifestream():
-    users = [
-        UserStatus(
-            login='11278', full_name='User1', balance=184.62, block_type=1,
-            last_block_start_date=1538342370, last_block_expire_date=datetime.datetime.now().timestamp(),
-            last_block_is_deleted=True, user_id=1146, lifestream_id='59e76927eea9ae0ce20c222e',
-            tarifs_id=[1311, 1517]
-        ),
-        UserStatus(
-            login='11272', full_name='User2', balance=184.62, block_type=1,
-            last_block_start_date=1538342370, last_block_expire_date=datetime.datetime.now().timestamp(),
-            last_block_is_deleted=True, user_id=1146, lifestream_id=None,
-            tarifs_id=[1311, 1517]
-        ),
-    ]
-    user_with_id_lifestream =  relation_utm_lifestream.get_users_utm_with_id_lifestream(users)
-    assert len(user_with_id_lifestream) == 1
+def test_get_users_utm_with_id_lifestream(utm_users):
+    user_with_id_lifestream = relation_utm_lifestream.get_users_utm_with_id_lifestream(utm_users)
+    assert len(user_with_id_lifestream) == 3
     assert user_with_id_lifestream[0].login == '11278'
-    assert user_with_id_lifestream[0].lifestream_id == '59e76927eea9ae0ce20c222e'
+    assert user_with_id_lifestream[0].lifestream_id == '567bc88ce2a6fe09ce000111'
 
 
 @pytest.fixture(
@@ -70,7 +86,7 @@ def id_lifestream_params(request):
 
 
 @pytest.fixture
-def lifestream_status_users():
+def lifestream_users():
     return [
         {
             'updated': '2018-10-09 08:01:07.197000+03:00',
@@ -94,25 +110,53 @@ def lifestream_status_users():
             'subscriptions': [], 'is_blocked': False,
             'created': '2015-12-24 13:27:24.189000+03:00'
         },
+        {
+            'updated': '2018-10-09 08:01:07.197000+03:00',
+            'id': '567bc88ce2a6fe09ce000113', 'city': '',
+            'email': 'anton@mail.ru', 'username': 'test1',
+            'info': {
+                'fio': 'Учетка для теста', 'activation_date': '24.12.15', 'period': '',
+                'address': 'г.Киров'
+            },
+            'subscriptions': [{'id': '110'}], 'is_blocked': False,
+            'created': '2015-12-24 13:27:24.189000+03:00'
+        },
     ]
 
 
-def test_get_lifestream_user(id_lifestream_params, lifestream_status_users):
+def test_get_lifestream_user(id_lifestream_params, lifestream_users):
     id_lifestream, result = id_lifestream_params
     user = relation_utm_lifestream.get_lifestream_user(
-        id_lifestream, lifestream_status_users)
+        id_lifestream, lifestream_users)
     assert bool(user) == result
 
 
-def test_get_lifestream_user_correct(lifestream_status_users):
+def test_get_lifestream_user_correct(lifestream_users):
     user = relation_utm_lifestream.get_lifestream_user(
-        '567bc88ce2a6fe09ce000112', lifestream_status_users)
+        '567bc88ce2a6fe09ce000112', lifestream_users)
     assert user['username'] == 'test2'
 
 
-def test_is_active_lifestream_user(lifestream_status_users):
-    user_0, user_1 = lifestream_status_users
+def test_is_active_lifestream_user(lifestream_users):
+    user_0, user_1, *_ = lifestream_users
     is_active = relation_utm_lifestream.is_active_lifestream_user(user_0)
     assert is_active is True
     is_active = relation_utm_lifestream.is_active_lifestream_user(user_1)
     assert is_active is False
+
+
+def test_find_change_status_to_lifestream(utm_users, lifestream_users):
+    change = relation_utm_lifestream.find_change_status_to_lifestream(
+        utm_users, lifestream_users)
+    assert len(change) == 2
+    assert change[0]['user'].full_name == 'User1'
+    assert change[0]['status_lifestream'] is True
+    assert change[0]['status_utm'] is False
+    assert len(change[0]['subscriptions']) == 1
+    assert change[0]['subscriptions'][0]['id'] == '109'
+
+    assert change[1]['user'].full_name == 'User3'
+    assert change[1]['status_lifestream'] is False
+    assert change[1]['status_utm'] is True
+    assert len(change[1]['subscriptions']) == 0
+    assert change[1]['user'].tarifs_id == [1311, 1517, 1716]
