df21b9249713b9250d58e4888f7a2b23b6b82b19
Dmitry Vologdin <ds.vologdin@yandex.ru>
2019-01-21 09:41:35 +0300

    update version of packets

--- lifestream.sh
+++ lifestream.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd `dirname $0`
-source env/bin/activate
+source venv/bin/activate
 python lifestream-utm/lifestream.py --log-level info  --email-report 'bud_on@kirovcity.ru i.solovyev@kgts.su' --log-file /var/log/lifestream.log
